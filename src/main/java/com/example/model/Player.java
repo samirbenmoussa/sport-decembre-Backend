503d265ad1900f1b444e8f135ccb3789c45578e0
firasbenyoussef <35664482+firasbenyoussef@users.noreply.github.com>
2021-01-28 20:06:42 +0100

    Player Crud
    adding Player crud Back End

---
+++ src/main/java/com/example/model/Player.java
@@ -0,0 +1,38 @@
+package com.example.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Player {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long idPlayer;
+	private String nom;
+	private String prenom;
+	public Long getIdPlayer() {
+		return idPlayer;
+	}
+	public void setIdPlayer(Long idPlayer) {
+		this.idPlayer = idPlayer;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	
+	
+	
+
+}
